03a84258671931af60fe79f80f138ff8f8957bcb
ORLA <orla850805@gmail.com>
2024-05-26 12:24:51 +0800

    update retry password

--- code/try_password.py
+++ code/try_password.py
@@ -1,12 +1,16 @@
-# 密碼輸入次數
-entry_time = 3
+entry_time = 3 # 密碼輸入次數
 password = 'a123456'
-while entry_time != 0:
+
+while entry_time > 0:
+    entry_time = entry_time - 1
     input_pwd = input('請輸入密碼：')
+    
     if input_pwd != password:
-        entry_time = entry_time - 1
-        if entry_time != 0:
-            print('密碼錯誤, 還有', entry_time, '次機會')
+        print('密碼錯誤!')
+        if entry_time > 0:
+            print('還有', entry_time, '次機會')
+        else:
+            print('帳號已被鎖')
     else:
         print('登入成功')
         break
