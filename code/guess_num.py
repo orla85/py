aac3df50c0917e72dbc5edd6540152860bd983b6
ORLA <orla850805@gmail.com>
2024-05-26 13:12:05 +0800

    update guess num

--- code/guess_num.py
+++ code/guess_num.py
@@ -1,12 +1,20 @@
 import random
 
-r = random.randint(1, 100)
+start = int(input('請輸入隨機數開始值：'))
+end = int(input('請輸入隨機數結束值：'))
+r = random.randint(start, end)
+cnt = 0 #猜測次數
+
 while True:
     num = int(input('猜猜數字：'))
+    cnt += 1
     if r > num:
         print('再大一點')
     elif r < num:
         print('再小一點')
     else:
         print('猜對了')
-        break    
+        print('目前猜了', cnt, '次') 
+        break
+    print('目前猜了', cnt, '次') 
+       
