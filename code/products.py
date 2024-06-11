58be0912fcee50429d26b46f8edc03a1595ff6a7
ORLA <orla850805@gmail.com>
2024-06-11 21:09:57 +0800

    write

--- code/products.py
+++ code/products.py
@@ -6,3 +6,7 @@ while True:
     price = input('請輸入商品價格：')
     products.append([name, price])
 print(products)
+
+with open('products.csv ','w') as f:
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
