891aeb5f96983e796ccd3562f61994912a85ba30
ORLA <orla850805@gmail.com>
2024-06-16 15:08:12 +0800

    add check file

--- code/products.py
+++ code/products.py
@@ -1,12 +1,19 @@
-#讀取檔案 
+import os # operating system
+
 products = []
-with open('products.csv','r',encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip().split(',')
-        products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): #檢查檔案是否存在
+    print('found')
+    #讀取檔案 
+    with open('products.csv','r',encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name,price])
+    print(products)
+else:
+    print('not found')
+
 
 #使用者輸入
 while True:
